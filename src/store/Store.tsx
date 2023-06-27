bffc9ea11580e58db75179b4ffb6b40586bbbe88
antonynans <antonynans@gmail.com>
2023-06-27 21:02:43 +0100

    added userinfo response to redux toolkit

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  settingSlice  from "./slice/setting";
 import loaderslice from './slice/loaderstate';
+import userinfoSlice from './slice/userinfo';
 
 const store = configureStore({
 	reducer: {
 		setting: settingSlice,
 		loaderslice:loaderslice,
+		userinfo: userinfoSlice,
 	},
 });
 
