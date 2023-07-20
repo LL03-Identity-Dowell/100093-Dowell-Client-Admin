7cbbe68f01e92abd66c5acf2ae3ffcedaad5b302
adebanjo23 <solomon.adebanjo@stu.cu.edu.ng>
2023-07-20 10:13:44 +0100

    Updated api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,20 +2,21 @@ from django.urls import path
 from .views import sessionView, OrgView, OrgsView, ProductsView, PublicLinkUpdate, updateOrg, portfolioview, getProduct, \
     GetDocumentProducts, update_products_client_admin, product_control_platform_admin, MemberControl, filter_portfolio, \
     settings, workspace_name, item_name, create_item, get_data, create_portfolio, create_role, get_layer_data, \
-    update_role_status, update_portfolio_status, update_item_status, save_device_layers, get_all_details
+    update_role_status, update_portfolio_status, update_item_status, save_device_layers, get_all_details, \
+    create_team_member
 
 urlpatterns = [
-    path('userinfo/', sessionView,name="userinfo"),
-    path('members/', OrgView,name="members"),
-    path('orgs/', OrgsView,name="orgs"),
-    path('public/', PublicLinkUpdate,name="public"),
-    path('products/', ProductsView,name="products"),
-    path('updateorg/',updateOrg,name="updateorg"),
-    path('mobileapi/',portfolioview,name="mobileapi"),
-    path('getproducts/',getProduct,name="getproducts"),
-    path('getdocumentproducts/',GetDocumentProducts,name="getdocumentproducts"),
-    path('clientadmin-products/',MemberControl,name="clientadmin-products"),
-    path('update-products-platform/',product_control_platform_admin,name="update-products-platform"),
+    path('userinfo/', sessionView, name="userinfo"),
+    path('members/', OrgView, name="members"),
+    path('orgs/', OrgsView, name="orgs"),
+    path('public/', PublicLinkUpdate, name="public"),
+    path('products/', ProductsView, name="products"),
+    path('updateorg/', updateOrg, name="updateorg"),
+    path('mobileapi/', portfolioview, name="mobileapi"),
+    path('getproducts/', getProduct, name="getproducts"),
+    path('getdocumentproducts/', GetDocumentProducts, name="getdocumentproducts"),
+    path('clientadmin-products/', MemberControl, name="clientadmin-products"),
+    path('update-products-platform/', product_control_platform_admin, name="update-products-platform"),
     path('get-portfolio/', filter_portfolio, name='get-portfolio'),
     path('settings/', settings, name='settings'),
     path('workspace_name/', workspace_name, name='workspace_name'),
@@ -30,5 +31,6 @@ urlpatterns = [
     path('update_item_status/', update_item_status, name='update_item_status'),
     path('save_device_layers/', save_device_layers, name='save_device_layers'),
     path('get_all_details/', get_all_details, name='get_all_details'),
+    path('create_team_member/', create_team_member, name='create_team_member'),
 
 ]
