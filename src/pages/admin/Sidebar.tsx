6ec4b689f229cfcbe6988bcf230a06c90d1d27c0
Nouman hayat <75477552+Nouman-hayat@users.noreply.github.com>
2023-07-28 11:09:40 +0500

    api implemented in siderbar mypofile section

--- src/pages/admin/Sidebar.tsx
+++ src/pages/admin/Sidebar.tsx
@@ -1,168 +1,296 @@
-import { AiFillCaretRight } from "react-icons/ai";
-import { useState } from 'react';
+
+
+
+
+import { useEffect, useState } from 'react';
+import Accordion from '../../components/accordion';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { getsidebarlastlogin, getsidebarworkspace } from '../../store/slice/sidebar';
+import { RootState } from '../../store/Store';
 
 const Sidebar = () => {
-  const [isSubmenuHidden, setSubmenuHidden] = useState(true);
+const adminusername = useSelector(
+	(state: RootState) => state.adminData.data[0].Username
+);
 
-  const toggleSubmenu = () => {
-    setSubmenuHidden(!isSubmenuHidden);
-  };
+	const [defaultusername, setdefaultusername] = useState(adminusername);
 
-  return (
-    <>
-      <div className="border border-black card-shadow lg:w-1/4 px-2 py-2">
-        <div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2]">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2">
-            My Profile
-          </h2>
-          <div className="card-shadow mx-4">
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem] mt-4">
-              <span
-                className="flex items-center h-12 gap-4 cursor-pointer"
-                onClick={toggleSubmenu}
-              >
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Workspaces</p>
-              </span>
+	useEffect(() => {
+		setdefaultusername(adminusername);
+		
+	}, [adminusername]);
 
-              <div
-                className={`text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold ${
-                  isSubmenuHidden ? "hidden" : ""
-                }`}
-                id="submenu"
-              >
-                {/* <h1 className="cursor-pointer p-2 hover:bg-[#cef9d2] rounded-md mt-1">
-                  Social
-                </h1>
-                <h1 className="cursor-pointer p-2 hover:bg-[#cef9d2] rounded-md mt-1">
-                  Personal
-                </h1>
-                <h1 className="cursor-pointer p-2 hover:bg-[#cef9d2] rounded-md mt-1">
-                  Friends
-                </h1> */}
-              </div>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Edit My Profile</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Last Logins</p>
-              </span>
-            </div>
-          </div>
-        </div>
+	
+	
+	const usedispatch = useDispatch()
+	useEffect(() => {
+		// Function to call the API
 
-        <div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2] mt-8">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2">
-            Portfolio Notifications
-          </h2>
-          <div className="card-shadow mx-4">
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem] mt-4">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">
-                  Team Member Chat (098)
-                </p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">User Chat (09)</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Public Chat (0045)</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">UX Living Lab Chat</p>
-              </span>
-            </div>
-          </div>
-        </div>
+		const fetchData = async () => {
+			try {
+				const os = {
+					username: defaultusername,
+				};
 
-        <div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2] mt-8">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2">
-            Announcements
-          </h2>
-          <div className="card-shadow mx-4">
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem] mt-4">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">To Team Members</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">To Users</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">To Public</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">From UX Living Lab</p>
-              </span>
-            </div>
-          </div>
-        </div>
+				
+				const osresponse = await axios.post(
+					"https://100093.pythonanywhere.com/api/get_workspaces/",
+					os
+				);
 
-        <div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2] mt-8">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2">
-            My Organisation
-          </h2>
-          <div className="card-shadow mx-4">
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem] mt-4">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Products (007)</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Team members (00025)</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Users (000025)</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Users (0000234)</p>
-              </span>
-            </div>
-            <div className="bg-[#7a7a7a] px-4  mb-[.1rem]">
-              <span className="flex items-center h-12 gap-4">
-                <AiFillCaretRight className="text-white" />
-                <p className="text-white font-semibold">Public (009)</p>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+				usedispatch(getsidebarworkspace(osresponse.data));
+
+				const os2 = {
+					username: defaultusername,
+				};
+
+				const os2response = await axios.post(
+					"https://100093.pythonanywhere.com/api/get_last_login/",
+					os2
+				);
+
+				usedispatch(getsidebarlastlogin(os2response.data));
+
+
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		// Call the API when the component mounts
+		fetchData();
+	}, [defaultusername]);
+
+	const workspace = useSelector(
+		(state: RootState) => state.sidebar?.workspace
+	);
+
+	const lastlogin = useSelector(
+		(state: RootState) => state.sidebar?.lastlogin[0]
+	);
+	return (
+		<>
+			<div className="border border-black card-shadow lg:w-1/4 px-2 py-2">
+				<div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						My Profile
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="Workspaces">
+							<div className="bg-[#CEF9D2] py-4 pl-8 box-border">
+								<ul>
+									{workspace.map((item, index) => (
+										<li
+											className="text-[#7a7a7a] text-lg font-medium"
+											key={index}
+										>
+											{index + 1}.{item}
+										</li>
+									))}
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="Edit My Profile">
+							<div className="bg-[#CEF9D2] py-4 pl-8 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										<a
+											href="http://100097.pythonanywhere.com/?session_id={{session_id}}"
+											className="text-lg font-medium"
+										>
+											1.Edit to Profile
+										</a>
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="Last Logins">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									{lastlogin.map((item, index) => (
+										<li
+											className="text-[#7a7a7a] text-lg font-medium break-words"
+											key={index}
+										>
+											{index + 1}.{item}
+										</li>
+									))}
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
+
+				<div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						Notifications
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="Team Member Chat ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="User Chat">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="Public Chat">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="UX Living Lab Chat">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
+
+				<div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						Announcements
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="To Team Member ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="To User ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="To Public ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="From UX Living Lab ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
+				<div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						My Organization
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="Products ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title=" Team Member ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title=" User ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title=" Public ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Sidebar;
