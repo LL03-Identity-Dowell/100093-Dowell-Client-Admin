7590e6e536d31af6a5ddbfd75e164f7a5697f97f
adebanjo23 <solomon.adebanjo@stu.cu.edu.ng>
2023-08-02 16:40:52 +0100

    Updated api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import sessionView, OrgView, OrgsView, ProductsView, PublicLinkUpdat
     GetDocumentProducts, update_products_client_admin, product_control_platform_admin, MemberControl, filter_portfolio, \
     settings, workspace_name, item_name, create_item, get_data, create_portfolio, create_role, get_layer_data, \
     update_role_status, update_portfolio_status, update_item_status, save_device_layers, get_all_details, \
-    create_team_member, create_user_member, get_workspaces, get_last_login, connect_portfolio
+    create_team_member, create_user_member, get_workspaces, get_last_login, connect_portfolio, otherorg
 
 urlpatterns = [
     path('userinfo/', sessionView, name="userinfo"),
@@ -36,4 +36,5 @@ urlpatterns = [
     path('get_workspaces/', get_workspaces, name='get_workspaces'),
     path('get_last_login/', get_last_login, name='get_last_login'),
     path('connect_portfolio/', connect_portfolio, name='connect_portfolio'),
+    path('otherorg/', otherorg, name='otherorg'),
 ]
