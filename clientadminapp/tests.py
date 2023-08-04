79a83390a83d26ba6ae022e6c4b25dd683c95739
Jazz365 <coolguyjazz365@gmail.com>
2023-08-04 17:30:12 +0530

    test case

--- clientadminapp/tests.py
+++ clientadminapp/tests.py
@@ -2,11 +2,13 @@ from django.test import TestCase
 import json
 from dowellconnection import dowellconnection
 # Create your tests here.
+import random
+import string
 
-# field1 = {}
-# login1 = dowellconnection("login", "bangalore", "login", "client_admin", "client_admin", "1159",
-#                             "ABCDE", "fetch", field1, "update")
-# r = json.loads(login1)
+field1 = {}
+login1 = dowellconnection("login", "bangalore", "login", "client_admin", "client_admin", "1159",
+                            "ABCDE", "fetch", field1, "update")
+r = json.loads(login1)
 # print(type(r))
 
 
@@ -37,3 +39,27 @@ from dowellconnection import dowellconnection
 
 # result = find_other_organisation_with_value(r)
 # print(result) 
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for i in range(length))
+
+org_id = "63cf8114554bd6bedf7b13ce"
+random_org_id = generate_random_string(len(org_id))
+new_org = {
+        "org_id": random_org_id,
+        "org_name": "HR_Dowell Research","username":"RainaMary","member_type":"team_members","product":"Team Management","data_type":"Real_Data","operations_right":"Add/Edit","role":"Employment In Dowell UXLivingLab","security_layer":"None","portfolio_name":"Dowell_Raina","portfolio_code":"229801","portfolio_specification":"","portfolio_uni_code":"229801","portfolio_details":"","status":"enable"}
+
+def get_other_organisation(document_name, documents,new_org):
+
+    for document in documents:
+        if document.get('document_name') == document_name:
+            document['other_organisation'].append(new_org)
+            print(document.get('other_organisation'))
+            field = {"document_name": document_name}
+            update = {"other_organisation":document.get('other_organisation')}
+            login = dowellconnection("login", "bangalore", "login", "client_admin", "client_admin", "1159",
+                                        "ABCDE", "update", field, update)
+            return document.get('other_organisation')
+    return None
+
+get_other_organisation("RainaMary",r["data"],new_org)
