8c2fd62127cb240d853ae5e088a419eab68a763d
nouman <nomi676767@gmail.com>
2023-08-11 21:46:40 +0500

    api added in product grid of other org

--- src/components/otherproducts/Products.tsx
+++ src/components/otherproducts/Products.tsx
@@ -7,9 +7,10 @@ import ProductForm from "./ProductForm";
 import { ToastContainer, toast } from "react-toastify";
 
 const Products = () => {
-	const productData = useSelector((state: RootState) => state.products);
-	const selectedorg = useSelector((state: RootState) => state.selectedorg.orgname);
+
+	const productData = useSelector((state: RootState) => state.otherorgdata);
 	const show_loader = useSelector((state: RootState) => state.loaderslice);
+	const selectedorgname = useSelector((state: RootState) => state.selectedorg.orgname);
 	const userData = useSelector((state: RootState) => state.userinfo);
 	const userName = userData.userinfo.username;
 
@@ -18,11 +19,9 @@ const Products = () => {
 	const [selectedProduct, setSelectedProduct] = useState<string>("");
 	const [selectedItem, setSelectedItem] = useState<string>("");
 
-	const portfolioData = useSelector(
-		(state: RootState) => state.adminData.data[0]?.portpolio
-	);
+	
 
-	const filterDataByProduct = portfolioData?.filter(
+	const filterDataByProduct = productData?.filter(
 		(item) => item.product === hovertitle
 	);
 
@@ -75,8 +74,11 @@ const Products = () => {
 				<div className="mt-8">
 					<div className="pl-8">
 						<p className="font-roboto text-lg text-[#7a7a7a] font-semibold my-8">
-							Products of <span className="text-[#FF0000]">{selectedorg}</span>,
-							Owner <span className="text-[#FF0000]">{selectedorg}</span>
+							Products of{" "}
+							<span className="text-[#FF0000]">{selectedorgname}</span>, Owner{" "}
+							<span className="text-[#FF0000]">
+								{selectedorgname} 
+							</span>
 						</p>
 						<p className="font-roboto text-lg text-[#7a7a7a] font-semibold">
 							Select product & Portfolio to connect
@@ -86,25 +88,19 @@ const Products = () => {
 					<section className="relative">
 						<main className={`flex flex-col w-full`}>
 							<div className="flex flex-wrap w-full justify-between">
-								{productData?.products?.length > 1 && (
+								{productData?.length > 0 && (
 									<>
-										{productData?.products?.map((product) => {
+										{productData?.map((product) => {
 											return (
 												<div
-													key={product._id}
+													key={product.org_id}
 													className="relative box last:flex-none hover:last:w-[53%]"
-													onMouseOver={() =>
-														handleMouseOver(product.product_name)
-													}
-													onMouseOut={() =>
-														handleMouseOut(product?.product_name)
-													}
-													onChange={() =>
-														setSelectedProduct(product.product_name)
-													}
+													onMouseOver={() => handleMouseOver(product.product)}
+													onMouseOut={() => handleMouseOut(product?.product)}
+													onChange={() => setSelectedProduct(product.product)}
 												>
 													<div className="h-80 w-80 relative">
-														<img
+														{/* <img
 															src={`${
 																product.product_logo?.includes(
 																	"https://100093.pythonanywhere.com"
@@ -114,50 +110,54 @@ const Products = () => {
 															} `}
 															alt=""
 															className="absolute w-full h-full"
+														/> */}
+														<img
+															src="https://uxlivinglab.com/wp-content/uploads/2022/12/Social-media-automation-2.png"
+															alt=""
+															className="absolute w-full h-full"
 														/>
 													</div>
-													{isHovering &&
-														product.product_name === hovertitle && (
-															<div className="absolute top-0 w-full h-full">
-																<form
-																	className="relative w-full h-full"
-																	onSubmit={handleSubmit}
-																>
-																	<div className="bg-[#a2a2a2] opacity-50 w-full h-full rounded-sm"></div>
-																	<div className="bg-transparent absolute w-full h-full top-0 text-center flex flex-col justify-around items-center">
-																		<h2 className="text-white text-[1.78rem] font-semibold">
-																			{product.product_name}
-																		</h2>
-																		<div className="w-full px-6">
-																			<select
-																				className="outline-none h-8 w-full"
-																				value={selectedItem}
-																				onChange={(e) =>
-																					setSelectedItem(e.target.value)
-																				}
-																			>
-																				<option> Select Portfolio </option>
-																				{filterDataByProduct?.map(
-																					(item, index) => (
-																						<option
-																							key={index}
-																							value={item?.portfolio_name}
-																						>
-																							{" "}
-																							{item?.portfolio_name},{" "}
-																							{item?.role}, {item?.data_type}
-																						</option>
-																					)
-																				)}
-																			</select>
-																		</div>
-																		<button className="bg-black text-white h-12 px-6 py-4 rounded-md flex items-center hover:bg-[#666666]">
-																			Connect
-																		</button>
+													{isHovering && product.product === hovertitle && (
+														<div className="absolute top-0 w-full h-full">
+															<form
+																className="relative w-full h-full"
+																onSubmit={handleSubmit}
+															>
+																<div className="bg-[#a2a2a2] opacity-50 w-full h-full rounded-sm"></div>
+																<div className="bg-transparent absolute w-full h-full top-0 text-center flex flex-col justify-around items-center">
+																	<h2 className="text-white text-[1.78rem] font-semibold">
+																		{product.product}
+																	</h2>
+																	<div className="w-full px-6">
+																		<select
+																			className="outline-none h-8 w-full"
+																			value={selectedItem}
+																			onChange={(e) =>
+																				setSelectedItem(e.target.value)
+																			}
+																		>
+																			<option> Select Portfolio </option>
+																			{filterDataByProduct?.map(
+																				(item, index) => (
+																					<option
+																						key={index}
+																						value={item?.portfolio_name}
+																					>
+																						{" "}
+																						{item?.portfolio_name}, {item?.role}
+																						, {item?.data_type}
+																					</option>
+																				)
+																			)}
+																		</select>
 																	</div>
-																</form>
-															</div>
-														)}
+																	<button className="bg-black text-white h-12 px-6 py-4 rounded-md flex items-center hover:bg-[#666666]">
+																		Connect
+																	</button>
+																</div>
+															</form>
+														</div>
+													)}
 												</div>
 											);
 										})}
