a885bd5f78aa44883d08a8b952087d16e4b5591f
nouman <nomi676767@gmail.com>
2023-08-25 18:54:03 +0500

    new button added in sidebar

--- src/pages/admin/Sidebar.tsx
+++ src/pages/admin/Sidebar.tsx
@@ -60,259 +60,267 @@ const Sidebar = () => {
     (state: RootState) => state.sidebar?.lastlogin[0]
   );
   return (
-    <>
-      <div className="border border-black card-shadow lg:w-1/4 px-2 py-2">
-        <div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2]">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
-            My Profile
-          </h2>
-          <div className="card-shadow mx-4">
-            <Accordion title="Workspaces">
-              <div className="bg-[#CEF9D2] py-4 pl-8 box-border">
-                <ul>
-                  {workspace.map((item, index) => (
-                    <li
-                      className="text-[#7a7a7a] text-lg font-medium"
-                      key={index}
-                    >
-                      {index + 1}.{item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="Edit My Profile">
-              <div className="bg-[#CEF9D2] py-4 pl-8 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    <a
-                      href="https://100097.pythonanywhere.com/?session_id={{session_id}}"
-                      className="text-lg font-medium"
-                    >
-                      1.Edit to Profile
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="Last Logins">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  {lastlogin.map((item, index) => (
-                    <li
-                      className="text-[#7a7a7a] text-lg font-medium break-words"
-                      key={index}
-                    >
-                      {index + 1}.{item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Accordion>
-          </div>
-        </div>
+		<>
+			<div className="border border-black card-shadow lg:w-1/4 px-2 py-2">
+				<div className="border border-[#61CE70] pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						My Profile
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="Workspaces">
+							<div className="bg-[#CEF9D2] py-4 pl-8 box-border">
+								<ul>
+									{workspace.map((item, index) => (
+										<li
+											className="text-[#7a7a7a] text-lg font-medium"
+											key={index}
+										>
+											{index + 1}.{item}
+										</li>
+									))}
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="Edit My Profile">
+							<div className="bg-[#CEF9D2] py-4 pl-8 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										<a
+											href="https://100097.pythonanywhere.com/?session_id={{session_id}}"
+											className="text-lg font-medium"
+										>
+											1.Edit to Profile
+										</a>
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="Last Logins">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									{lastlogin.map((item, index) => (
+										<li
+											className="text-[#7a7a7a] text-lg font-medium break-words"
+											key={index}
+										>
+											{index + 1}.{item}
+										</li>
+									))}
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
 
-        <div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
-            Notifications
-          </h2>
-          <div className="card-shadow mx-4">
-            <Accordion title="Team Member Chat ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="User Chat">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="Public Chat">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="UX Living Lab Chat">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-          </div>
-        </div>
+				<div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						Notifications
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="Team Member Chat ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="User Chat">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="Public Chat">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="UX Living Lab Chat">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
 
-        <div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
-            Announcements
-          </h2>
-          <div className="card-shadow mx-4">
-            <Accordion title="To Team Member ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="To User ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="To Public ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title="From UX Living Lab ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-          </div>
-        </div>
-        <div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
-          <h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
-            My Organization
-          </h2>
-          <div className="card-shadow mx-4">
-            <Accordion title="Products ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title=" Team Member ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title=" User ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-            <Accordion title=" Public ">
-              <div className="bg-[#CEF9D2] p-4 box-border">
-                <ul>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                  <li className="text-[#7a7a7a] text-lg font-medium">
-                    1.Organization 1
-                  </li>
-                </ul>
-              </div>
-            </Accordion>
-          </div>
-        </div>
+				<div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						Announcements
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="To Team Member ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="To User ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="To Public ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title="From UX Living Lab ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
+				<div className="border border-[#61CE70] mt-8 pb-2 hover:bg-[#cef9d2]">
+					<h2 className="text-[19px] font-semibold text-[#61CE70] px-4 mt-2 mb-4">
+						My Organization
+					</h2>
+					<div className="card-shadow mx-4">
+						<Accordion title="Products ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title=" Team Member ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title=" User ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+						<Accordion title=" Public ">
+							<div className="bg-[#CEF9D2] p-4 box-border">
+								<ul>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+									<li className="text-[#7a7a7a] text-lg font-medium">
+										1.Organization 1
+									</li>
+								</ul>
+							</div>
+						</Accordion>
+					</div>
+				</div>
 
-        <section className="card-shadow border-black border mt-8 bg-[#cef9d2]">
-          <div className="border-b border-black p-1">
-            <button
-              className="border border-[#ff0036] text-[#ff0036] text-[13px] font-roboto rounded-[5px] px-5 py-[10px] hover:bg-[#61ce70] hover:text-white hover:border-white"
-              onClick={() =>
-                (window.location.href =
-                  "https://100093.pythonanywhere.com/upload")
-              }
-            >
-              Upload
-            </button>
-          </div>
-          <div className="flex justify-between items-center border-t border-black p-1">
-            <button
-              className="border border-[#ff0036] text-[#ff0036] text-[13px] font-roboto rounded-[5px] px-5 py-[10px] hover:bg-[#61ce70] hover:text-white hover:border-white"
-              onClick={() =>
-                (window.location.href = `https://ll03-identity-dowell.github.io/100096-DowellChat/#/living-lab-chat/?session_id=${sessionId}`)
-              }
-            >
-              Customer Support
-            </button>
-            <button
-              className="border border-[#ff0036] text-[#ff0036] text-[13px] font-roboto rounded-[5px] px-5 py-[10px] hover:bg-[#61ce70] hover:text-white hover:border-white"
-              onClick={() =>
-                (window.location.href = "https://100093.pythonanywhere.com/#")
-              }
-            >
-              Error Reporting
-            </button>
-          </div>
-        </section>
-      </div>
-    </>
-  );
+				<div className="button-container flex justify-between text-center border-black border p-[2px] w-full bg-[#CEF9D2]">
+					<a
+						href="https://100093.pythonanywhere.com/upload"
+						className="m-[5px] px-[20px] py-[10px] bg-transparent border-[red] text-[red] w-[45%] text-center leading-[15px] text-[12px] font-semibold rounded no-underline border"
+					>
+						Upload
+					</a>
+				</div>
+				<div className="button-container flex justify-between text-center border-black border p-[2px] w-full bg-[#CEF9D2]">
+					<a
+						href="https://ll03-identity-dowell.github.io/100096-DowellChat/#/living-lab-chat/?session_id={{session_id}}"
+						className="m-[5px] px-[20px] py-[10px] bg-transparent border-[red] text-[red] w-[45%] text-center leading-[15px] text-[12px] font-semibold rounded no-underline border"
+					>
+						Customer Support
+					</a>
+					<a
+						href="#"
+						className="m-[5px] px-[20px] py-[10px] bg-transparent border-[red] text-[red] w-[45%] text-center leading-[15px] text-[12px] font-semibold rounded no-underline border"
+					>
+						Error Reporting
+					</a>
+				</div>
+
+				<div className="button-container flex justify-between text-center border-black border p-[2px] w-full bg-[#CEF9D2]">
+					<a
+						href="#"
+						className="m-[5px] px-[20px] py-[10px] bg-transparent border-[red] text-[red] w-[45%] text-center leading-[15px] text-[12px] font-semibold rounded no-underline border"
+					>
+						Buy/Redeem Credits
+					</a>
+					<a
+						href="https://chrome.google.com/webstore/detail/dowell-ux-living-lab/acnnapiadbgagcnidgnclaohnpmbpebl?utm_source=ext_app_menu"
+						className="m-[5px] px-[20px] py-[10px] bg-transparent border-[red] text-[red] w-[45%] text-center leading-[15px] text-[12px] font-semibold rounded no-underline border"
+					>
+						Chrome Extension
+					</a>
+				</div>
+
+				
+			</div>
+		</>
+	);
 };
 
 export default Sidebar;
