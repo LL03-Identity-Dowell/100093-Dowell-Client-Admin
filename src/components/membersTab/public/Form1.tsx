1cd309c1a4a73b483f6203e15a993ce21e1f1f80
antonynans <antonynans@gmail.com>
2023-09-15 17:06:34 +0100

    added public members api

--- src/components/membersTab/public/Form1.tsx
+++ src/components/membersTab/public/Form1.tsx
@@ -1,18 +1,72 @@
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/Store";
+import { toast } from "react-toastify";
+
 const Form1 = () => {
+  const [formInputs, setFormInputs] = useState({
+    public_count: 0,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const userName = useSelector(
+    (state: RootState) => state.adminData.data[0]?.Username
+  );
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormInputs({ ...formInputs, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const data = {
+      username: userName,
+      present_org: userName,
+      public_count: formInputs.public_count,
+    };
+
+    try {
+      await axios
+        .post(
+          "https://100093.pythonanywhere.com/api/create_public_member/",
+          data
+        )
+        .then((res) => {
+          console.log(res.data);
+          toast.success("success");
+        });
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.error(error);
+        toast.error(error.message);
+      } else {
+        console.error("An unknown error occurred:", error);
+        toast.error("An unknown error occurred");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <div className="lg:w-1/3 border border-[#54595F] card-shadow">
         <p className="text-[#FF0000] text-lg font-roboto font-semibold p-[30px] m-5 flex flex-col items-center">
           Invite PUBLIC to my organisation
         </p>
-        <form className="px-[30px] mb-8">
+        <form className="px-[30px] mb-8" onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
               Number of Public Links needed now
             </label>
             <input
-              type="text"
+              type="number"
               placeholder="Number"
+              id="public_count"
+              onChange={handleOnChange}
               className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
             />
           </div>
@@ -30,8 +84,14 @@ const Form1 = () => {
               <option>1000 </option>
             </select>
           </div>
-          <button className="w-full h-12 bg-[#7a7a7a] hover:bg-[#61CE70] rounded-[4px] text-white font-roboto">
-            Create Public Invitation Link
+          <button
+            disabled={isLoading}
+            className={`w-full h-12 bg-[#7a7a7a] hover:bg-[#61CE70] rounded-[4px] text-white font-roboto ${
+              isLoading ? "hover:bg-[#7a7a7a] opacity-50" : ""
+            }`}
+          >
+            {" "}
+            {isLoading ? "loading..." : "Create Public Invitation Link"}
           </button>
         </form>
       </div>
