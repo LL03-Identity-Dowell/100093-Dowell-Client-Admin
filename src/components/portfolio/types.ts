cf68d3365b856d11a6e4b4d5c46d406d9d018369
antonynans <antonynans@gmail.com>
2023-10-04 19:53:25 +0100

    added loading state to the enable disable portfolio

---
+++ src/components/portfolio/types.ts
@@ -0,0 +1,24 @@
+export type FormInputs = {
+  username: string;
+  member_type: string;
+  member: string[];
+  product: string;
+  data_type: string;
+  op_rights: string;
+  role: string;
+  portfolio_name: string;
+  portfolio_code: string;
+  portfolio_status: string;
+  portfolio_spec: string;
+  portfolio_u_code: string;
+  portfolio_det: string;
+};
+
+export interface PublicResponse {
+  id: string;
+}
+
+export interface Option {
+  value: string;
+  label: string;
+}
