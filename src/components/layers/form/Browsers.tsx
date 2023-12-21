7caee1033a9a2b1e3f6de4b0c09bb7deaf85b3eb
temiyores <temiyores1234@gmail.com>
2023-12-21 22:36:15 +0100

    added select layer default text to all forms in layers tab

--- src/components/layers/form/Browsers.tsx
+++ src/components/layers/form/Browsers.tsx
@@ -138,152 +138,187 @@ export default function Browsers() {
     // Make your API call here using the selectedLanguage value
     // For example:
   };
- const color_scheme = useSelector(
-		(state: RootState) => state.setting?.data?.color_scheme
- );
+  const color_scheme = useSelector(
+    (state: RootState) => state.setting?.data?.color_scheme
+  );
   return (
-		<>
-			<ToastContainer position="top-right" />
-			<form className="px-[30px] mb-8">
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Chrome
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedChrome(e.target.value)}
-					>
-						<option selected value={Chrome}>
-							{Chrome}
-						</option>
-						{Chrome_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Safari
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedSafari(e.target.value)}
-					>
-						<option selected value={Safari}>
-							{Safari}
-						</option>
-						{Safari_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Bing
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedBing(e.target.value)}
-					>
-						<option selected value={Bing}>
-							{Bing}
-						</option>
-						{Bing_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Firefox
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedFirefox(e.target.value)}
-					>
-						<option selected value={Firefox}>
-							{Firefox}
-						</option>
-						{Firefox_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Edge
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedEdge(e.target.value)}
-					>
-						<option selected value={Edge}>
-							{Edge}
-						</option>
-						{Edge_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Opera
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedOpera(e.target.value)}
-					>
-						<option selected value={Opera}>
-							{Opera}
-						</option>
-						{Opera_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#756464] text-lg font-roboto font-bold ">
-						Others not listed above
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedOthers(e.target.value)}
-					>
-						<option selected value={Others}>
-							{Others}
-						</option>
-						{Others_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<button
-					className={`w-full ${
-						color_scheme == "Red"
-							? "bg-[#DC4C64]"
-							: color_scheme == "Green"
-							? "bg-[#14A44D]"
-							: "bg-[#7A7A7A]"
-					}  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md`}
-					onClick={handleSubmitbrowser}
-				>
-					Save Browser Settings
-				</button>
-			</form>
-		</>
-	);
+    <>
+      <ToastContainer position="top-right" />
+      <form className="px-[30px] mb-8">
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Chrome
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedChrome(e.target.value)}
+          >
+            <option disabled={Boolean(Chrome)} selected>
+              …select security layer…
+            </option>
+            {Chrome && (
+              <option selected value={Chrome}>
+                {Chrome}
+              </option>
+            )}
+            {Chrome_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Safari
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedSafari(e.target.value)}
+          >
+            <option disabled={Boolean(Safari)} selected>
+              …select security layer…
+            </option>
+            {Safari && (
+              <option selected value={Safari}>
+                {Safari}
+              </option>
+            )}
+            {Safari_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Bing
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedBing(e.target.value)}
+          >
+            <option disabled={Boolean(Bing)} selected>
+              …select security layer…
+            </option>
+            {Bing && (
+              <option selected value={Bing}>
+                {Bing}
+              </option>
+            )}
+            {Bing_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Firefox
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedFirefox(e.target.value)}
+          >
+            <option disabled={Boolean(Firefox)} selected>
+              …select security layer…
+            </option>
+            {Firefox && (
+              <option selected value={Firefox}>
+                {Firefox}
+              </option>
+            )}
+            {Firefox_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Edge
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedEdge(e.target.value)}
+          >
+            <option disabled={Boolean(Edge)} selected>
+              …select security layer…
+            </option>
+            {Edge && (
+              <option selected value={Edge}>
+                {Edge}
+              </option>
+            )}
+            {Edge_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Opera
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedOpera(e.target.value)}
+          >
+            <option disabled={Boolean(Opera)} selected>
+              …select security layer…
+            </option>
+            {Opera && (
+              <option selected value={Opera}>
+                {Opera}
+              </option>
+            )}
+            {Opera_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#756464] text-lg font-roboto font-bold ">
+            Others not listed above
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedOthers(e.target.value)}
+          >
+            <option disabled={Boolean(Others)} selected>
+              …select security layer…
+            </option>
+            {Others && (
+              <option selected value={Others}>
+                {Others}
+              </option>
+            )}
+            {Others_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button
+          className={`w-full ${
+            color_scheme == "Red"
+              ? "bg-[#DC4C64]"
+              : color_scheme == "Green"
+              ? "bg-[#14A44D]"
+              : "bg-[#7A7A7A]"
+          }  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md`}
+          onClick={handleSubmitbrowser}
+        >
+          Save Browser Settings
+        </button>
+      </form>
+    </>
+  );
 }
--- src/components/layers/form/Connectiontype.tsx
+++ src/components/layers/form/Connectiontype.tsx
@@ -1,219 +1,236 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../store/Store";
 import { useEffect, useState } from "react";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import axios from "axios";
 import { getlayercontype } from "../../../store/slice/layers";
-import { toast } from 'react-toastify';
-
+import { toast } from "react-toastify";
 
 export default function Connectiontype() {
+  const adminusername = useSelector(
+    (state: RootState) => state.userinfo.userinfo.username
+  );
 
-	const adminusername = useSelector(
-		(state: RootState) => state.userinfo.userinfo.username
-	);
+  const [defaultusername, setdefaultusername] = useState(adminusername);
 
-	const [defaultusername, setdefaultusername] = useState(adminusername);
+  useEffect(() => {
+    setdefaultusername(adminusername);
+  }, [adminusername]);
 
-	useEffect(() => {
-		setdefaultusername(adminusername);
-	}, [adminusername]);
+  const layerlist = [
+    "layer1",
+    "layer2",
+    "layer3",
+    "layer4",
+    "layer5",
+    "layer6",
+  ];
 
-	const layerlist = ["layer1", "layer2", "layer3", "layer4", "layer5", "layer6"];
+  const Others = useSelector(
+    (state: RootState) =>
+      state.layer.con_type?.["Others not listed above"] || ""
+  );
 
+  const [selectedOthers, setselectedOthers] = useState(Others);
+  const Others_filterlist = layerlist.filter((item) => item !== Others);
 
-	const Others = useSelector(
-		(state: RootState) => state.layer.con_type?.["Others not listed above"] || ""
-	);
+  const OfficeWifi = useSelector(
+    (state: RootState) =>
+      state.layer.con_type?.["Office Wifi/Secured Wifi"] || ""
+  );
 
-	const [selectedOthers, setselectedOthers] = useState(Others);
-	const Others_filterlist = layerlist.filter((item) => item !== Others);
+  const [selectedOfficeWifi, setselectedOfficeWifi] = useState(OfficeWifi);
+  const OfficeWifi_filterlist = layerlist.filter((item) => item !== OfficeWifi);
 
+  const PublicWifi = useSelector(
+    (state: RootState) => state.layer.con_type?.["Public Wifi"] || ""
+  );
 
+  const [selectedPublicWifi, setselectedPublicWifi] = useState(PublicWifi);
+  const PublicWifi_filterlist = layerlist.filter((item) => item !== PublicWifi);
 
-	const OfficeWifi = useSelector(
-		(state: RootState) => state.layer.con_type?.["Office Wifi/Secured Wifi"] || ""
-	);
+  const MobileData = useSelector(
+    (state: RootState) => state.layer.con_type?.["Mobile Data"] || ""
+  );
 
-	const [selectedOfficeWifi, setselectedOfficeWifi] = useState(OfficeWifi);
-	const OfficeWifi_filterlist = layerlist.filter((item) => item !== OfficeWifi);
+  const [selectedMobileData, setselectedMobileData] = useState(MobileData);
+  const MobileData_filterlist = layerlist.filter((item) => item !== MobileData);
 
+  useEffect(() => {
+    setselectedOthers(Others);
+    setselectedMobileData(MobileData);
+    setselectedOfficeWifi(OfficeWifi);
+    setselectedPublicWifi(PublicWifi);
+  }, [Others, MobileData, OfficeWifi, PublicWifi]);
 
+  const dispatch = useDispatch();
+  const handleSubmit = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
 
-	const PublicWifi = useSelector(
-		(state: RootState) => state.layer.con_type?.["Public Wifi"] || ""
-	);
+    const postData = async () => {
+      const data = {
+        username: defaultusername,
+        category: "connection_type",
+        data: {
+          "Mobile Data": selectedMobileData,
+          "Office Wifi/Secured Wifi": selectedOfficeWifi,
+          "Public Wifi": selectedPublicWifi,
+          "Others not listed above": selectedOthers,
+        },
+      };
 
-	const [selectedPublicWifi, setselectedPublicWifi] = useState(PublicWifi);
-	const PublicWifi_filterlist = layerlist.filter((item) => item !== PublicWifi);
+      try {
+        // dispatch(getloaderstate(true));
 
+        await axios.post(
+          "https://100093.pythonanywhere.com/api/save_device_layers/",
+          data
+        );
 
-	const MobileData = useSelector(
-		(state: RootState) => state.layer.con_type?.["Mobile Data"] || ""
-	);
+        dispatch(
+          getlayercontype({
+            "Mobile Data": selectedMobileData,
+            "Office Wifi/Secured Wifi": selectedOfficeWifi,
+            "Public Wifi": selectedPublicWifi,
+            "Others not listed above": selectedOthers,
+          })
+        );
+        toast.success("success");
+        // dispatch(getloaderstate(false));
+      } catch (error) {
+        console.error(error);
+      }
 
-	const [selectedMobileData, setselectedMobileData] = useState(MobileData);
-	const MobileData_filterlist = layerlist.filter((item) => item !== MobileData);
+      // fetch product
+    };
 
-	useEffect(() => {
-		setselectedOthers(Others);
-		setselectedMobileData(MobileData);
-		setselectedOfficeWifi(OfficeWifi);
-		setselectedPublicWifi(PublicWifi);
-	}, [Others, MobileData, OfficeWifi, PublicWifi]);
+    // Call the API when the component mounts
+    postData();
 
+    // Make your API call here using the selectedLanguage value
+    // For example:
+  };
 
+  const color_scheme = useSelector(
+    (state: RootState) => state.setting?.data?.color_scheme
+  );
 
-	const dispatch = useDispatch();
-	const handleSubmit = (
-		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-	) => {
-		event.preventDefault();
+  return (
+    <>
+      <ToastContainer position="top-right" />
+      <form className="px-[30px] mb-8">
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Mobile Data
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedMobileData(e.target.value)}
+            value={selectedMobileData}
+          >
+            <option disabled={Boolean(MobileData)} selected>
+              …select security layer…
+            </option>
+            {MobileData && (
+              <option selected value={MobileData}>
+                {MobileData}
+              </option>
+            )}
+            {MobileData_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Office Wifi/Secured Wifi
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedOfficeWifi(e.target.value)}
+            value={selectedOfficeWifi}
+          >
+            <option disabled={Boolean(OfficeWifi)} selected>
+              …select security layer…
+            </option>
+            {OfficeWifi && (
+              <option selected value={OfficeWifi}>
+                {OfficeWifi}
+              </option>
+            )}
+            {OfficeWifi_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Public Wifi
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedPublicWifi(e.target.value)}
+            value={selectedPublicWifi}
+          >
+            <option disabled={Boolean(PublicWifi)} selected>
+              …select security layer…
+            </option>
+            {PublicWifi && (
+              <option selected value={PublicWifi}>
+                {PublicWifi}
+              </option>
+            )}
+            {PublicWifi_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
 
-		const postData = async () => {
-			const data = {
-				username: defaultusername,
-				category: "connection_type",
-				data: {
-					"Mobile Data": selectedMobileData,
-					"Office Wifi/Secured Wifi": selectedOfficeWifi,
-					"Public Wifi": selectedPublicWifi,
-					"Others not listed above": selectedOthers,
-				},
-			};
-
-			try {
-				// dispatch(getloaderstate(true));
-
-				await axios.post(
-					"https://100093.pythonanywhere.com/api/save_device_layers/",
-					data
-				);
-
-				dispatch(
-					getlayercontype({
-						"Mobile Data": selectedMobileData,
-						"Office Wifi/Secured Wifi": selectedOfficeWifi,
-						"Public Wifi": selectedPublicWifi,
-						"Others not listed above": selectedOthers,
-					})
-				);
-				toast.success("success");
-				// dispatch(getloaderstate(false));
-			} catch (error) {
-				console.error(error);
-			}
-
-			// fetch product
-		};
-
-		// Call the API when the component mounts
-		postData();
-
-		// Make your API call here using the selectedLanguage value
-		// For example:
-	};
-
-
- const color_scheme = useSelector(
-		(state: RootState) => state.setting?.data?.color_scheme
- );
-
-
-	return (
-		<>
-			<ToastContainer position="top-right" />
-			<form className="px-[30px] mb-8">
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Mobile Data
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedMobileData(e.target.value)}
-						value={selectedMobileData}
-					>
-						<option selected value={MobileData}>
-							{MobileData}
-						</option>
-						{MobileData_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Office Wifi/Secured Wifi
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedOfficeWifi(e.target.value)}
-						value={selectedOfficeWifi}
-					>
-						<option selected value={OfficeWifi}>
-							{OfficeWifi}
-						</option>
-						{OfficeWifi_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Public Wifi
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedPublicWifi(e.target.value)}
-						value={selectedPublicWifi}
-					>
-						<option selected value={PublicWifi}>
-							{PublicWifi}
-						</option>
-						{PublicWifi_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-
-				<div className="mb-4">
-					<label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
-						Others not listed above
-					</label>
-					<select
-						className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-						onChange={(e) => setselectedOthers(e.target.value)}
-						value={selectedOthers}
-					>
-						<option selected value={Others}>
-							{Others}
-						</option>
-						{Others_filterlist.map((item, index) => (
-							<option key={index} value={item}>
-								{item}
-							</option>
-						))}
-					</select>
-				</div>
-				<button
-					className={`w-full ${
-						color_scheme == "Red"
-							? "bg-[#DC4C64]"
-							: color_scheme == "Green"
-							? "bg-[#14A44D]"
-							: "bg-[#7A7A7A]"
-					}  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md`}
-					onClick={handleSubmit}
-				>
-					Save Internet Settings
-				</button>
-			</form>
-		</>
-	);
+        <div className="mb-4">
+          <label className="text-[#7A7A7A] text-lg font-roboto font-bold ">
+            Others not listed above
+          </label>
+          <select
+            className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+            onChange={(e) => setselectedOthers(e.target.value)}
+            value={selectedOthers}
+          >
+            <option disabled={Boolean(Others)} selected>
+              …select security layer…
+            </option>
+            {Others && (
+              <option selected value={Others}>
+                {Others}
+              </option>
+            )}
+            {Others_filterlist.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button
+          className={`w-full ${
+            color_scheme == "Red"
+              ? "bg-[#DC4C64]"
+              : color_scheme == "Green"
+              ? "bg-[#14A44D]"
+              : "bg-[#7A7A7A]"
+          }  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md`}
+          onClick={handleSubmit}
+        >
+          Save Internet Settings
+        </button>
+      </form>
+    </>
+  );
 }
