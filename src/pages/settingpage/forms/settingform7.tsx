a2956abe261ed29851b37c776a345094bdaffb5e
temiyores <temiyores1234@gmail.com>
2023-12-31 14:35:11 +0100

    api key hidden

--- src/pages/settingpage/forms/settingform7.tsx
+++ src/pages/settingpage/forms/settingform7.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import { getsetting } from "../../../store/slice/setting";
 import { toast } from "react-toastify";
 import { Ids, SelectIds } from "../../../Ids";
+import Loader from "../../whiteloader";
+import { getloaderstate } from "../../../store/slice/loaderstate";
 
 const Settingform7 = () => {
   const defaultlang = useSelector(
@@ -13,6 +15,7 @@ const Settingform7 = () => {
   const currentSetting = useSelector((state: RootState) => state.setting?.data);
   const [isLoading, setIsLoading] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState(defaultlang);
+  const [loadingstate, setLoadingState] = useState(false);
 
   const adminusername = useSelector(
     (state: RootState) => state.userinfo.userinfo.username
@@ -72,7 +75,6 @@ const Settingform7 = () => {
   const color_scheme = useSelector(
     (state: RootState) => state.setting?.data?.color_scheme
   );
-
   const languageMapping: { [key: string]: string } = {
     af: "Afrikaans",
     sq: "Albanian",
@@ -182,145 +184,166 @@ const Settingform7 = () => {
     yo: "Yoruba",
     zu: "Zulu",
   };
-  const API_KEY = "AIzaSyDy72jLQFTZ9TkEXjjZLoJE6mbAcjjMAuM";
   const languages = Object.entries(languageMapping);
   useEffect(() => {
-    console.log(selectedLanguage);
-    SelectIds.forEach((id: string) => {
-      const select = document.getElementById(id) as HTMLSelectElement | null;
-      // Accessing individual options
-      if (select) {
-        const options = select.options;
-        for (let i = 0; i < options.length; i++) {
+    const FetchLanguage = () => {
+      SelectIds.forEach((id: string) => {
+        const select = document.getElementById(id) as HTMLSelectElement | null;
+        // Accessing individual options
+        if (select) {
+          setLoadingState(true);
+          const options = select.options;
+          for (let i = 0; i < options.length; i++) {
+            const translate = async () => {
+              try {
+                const data = {
+                  text: options[i].text,
+                  target_language: selectedLanguage,
+                };
+                const response = await axios.post(
+                  `https://100093.pythonanywhere.com/api/translate/`,
+                  data
+                );
+
+                const translationData = await response.data;
+                if (id === "settingForm2text1") {
+                  console.log(translationData);
+                }
+                options[i].text =
+                  translationData.data.translations[0].translatedText;
+                setLoadingState(false);
+              } catch (error) {
+                console.error("Translation error:", error);
+                setLoadingState(false);
+                return options[i].text;
+              }
+            };
+            translate();
+          }
+        }
+      });
+      Ids.forEach((id: string) => {
+        const text = document.getElementById(id);
+        if (text) {
+          setLoadingState(true);
           const translate = async () => {
             try {
-              const response = await fetch(
-                `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&q=${encodeURIComponent(
-                  options[i].text
-                )}&target=${selectedLanguage}`,
-                {
-                  method: "POST",
-                }
+              const data = {
+                text: text.innerText,
+                target_language: selectedLanguage,
+              };
+              const response = await axios.post(
+                `https://100093.pythonanywhere.com/api/translate/`,
+                data
               );
-              const translationData = await response.json();
-              options[i].text =
+
+              const translationData = await response.data;
+              text.innerText =
                 translationData.data.translations[0].translatedText;
+              setLoadingState(false);
             } catch (error) {
               console.error("Translation error:", error);
-              return options[i].text;
+              setLoadingState(false);
+              return text;
             }
           };
           translate();
         }
-      }
-    });
+      });
+    };
 
-    Ids.forEach((id: string) => {
-      const text = document.getElementById(id);
-      if (text) {
-        const translate = async () => {
-          try {
-            const response = await fetch(
-              `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&q=${encodeURIComponent(
-                text.innerHTML
-              )}&target=${selectedLanguage}`,
-              {
-                method: "POST",
-              }
-            );
-            const translationData = await response.json();
-            text.innerText =
-              translationData.data.translations[0].translatedText;
-          } catch (error) {
-            console.error("Translation error:", error);
-            return text;
-          }
-        };
-        translate();
-      }
-    });
-  }, [selectedLanguage]);
+    if (selectedLanguage) {
+      FetchLanguage();
+    }
+  }, [selectedLanguage, dispatch]);
+  console.log({ loadingstate });
   return (
     <div className="form-item">
-      <div
-        id="settingForm7text1"
-        className={`${
-          color_scheme == "Red"
-            ? "bg-[lightcoral]"
-            : color_scheme == "Green"
-            ? "bg-[lightgreen]"
-            : "bg-[#a1a1a1] "
-        } p-3 text-[18px] font-semibold text-white border-[1px] border-[#61CE70] ${
-          color_scheme == "Red"
-            ? "border-[#DC4C64]"
-            : color_scheme == "Green"
-            ? "border-[#14A44D]"
-            : "border-[#7A7A7A]"
-        } border-solid`}
-      >
-        Set Language for owner
-      </div>
-      <form
-        action=""
-        className={` p-3 border-[1px] ${
-          color_scheme == "Red"
-            ? "border-[#DC4C64]"
-            : color_scheme == "Green"
-            ? "border-[#14A44D]"
-            : "border-[#7A7A7A]"
-        } border-solid`}
-      >
-        <div className="w-full mb-3">
-          <label
-            id="settingForm7text2"
-            htmlFor="languagesSelect"
-            className="text-[18px] font-semibold text-[#7A7A7A]"
-          >
-            Languages
-          </label>
-          <select
-            id="languagesSelect"
-            className="w-full p-1 text-[17px] font-medium text-[#7A7A7A] border-[1px] border-[#7A7A7A] border-solid bg-[#F5F5F5] focus:outline-none rounded-md"
-            value={selectedLanguage}
-            onChange={(e) => setSelectedLanguage(e.target.value)}
-            aria-label="Default select example"
-          >
-            {languages.map(([code, name]) => {
-              if (name === defaultlang || code === defaultlang) {
-                return (
-                  <option key={code} value={code}>
-                    {name}
-                  </option>
-                );
-              } else {
-                return (
-                  <option key={code} value={code}>
-                    {name}
-                  </option>
-                );
-              }
-            })}
-          </select>
-        </div>
-
-        <div className="w-full mb-1">
-          <button
-            id="settingForm7text3"
-            className={`w-full ${
+      {loadingstate ? (
+        <Loader></Loader>
+      ) : (
+        <>
+          <div
+            id="settingForm7text1"
+            className={`${
               color_scheme == "Red"
-                ? "bg-[#DC4C64]"
+                ? "bg-[lightcoral]"
                 : color_scheme == "Green"
-                ? "bg-[#14A44D]"
-                : "bg-[#7A7A7A]"
-            }  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md ${
-              isLoading ? "opacity-50" : null
-            }`}
-            onClick={handleSubmit}
+                ? "bg-[lightgreen]"
+                : "bg-[#a1a1a1] "
+            } p-3 text-[18px] font-semibold text-white border-[1px] border-[#61CE70] ${
+              color_scheme == "Red"
+                ? "border-[#DC4C64]"
+                : color_scheme == "Green"
+                ? "border-[#14A44D]"
+                : "border-[#7A7A7A]"
+            } border-solid`}
           >
-            {isLoading ? "saving..." : "Set as default Language for me"}
-          </button>
-        </div>
-      </form>
+            Set Language for owner
+          </div>
+          <form
+            action=""
+            className={` p-3 border-[1px] ${
+              color_scheme == "Red"
+                ? "border-[#DC4C64]"
+                : color_scheme == "Green"
+                ? "border-[#14A44D]"
+                : "border-[#7A7A7A]"
+            } border-solid`}
+          >
+            <div className="w-full mb-3">
+              <label
+                id="settingForm7text2"
+                htmlFor="languagesSelect"
+                className="text-[18px] font-semibold text-[#7A7A7A]"
+              >
+                Languages
+              </label>
+              <select
+                id="languagesSelect"
+                className="w-full p-1 text-[17px] font-medium text-[#7A7A7A] border-[1px] border-[#7A7A7A] border-solid bg-[#F5F5F5] focus:outline-none rounded-md"
+                value={selectedLanguage}
+                onChange={(e) => setSelectedLanguage(e.target.value)}
+                aria-label="Default select example"
+              >
+                {languages.map(([code, name]) => {
+                  if (name === defaultlang || code === defaultlang) {
+                    return (
+                      <option key={code} value={code}>
+                        {name}
+                      </option>
+                    );
+                  } else {
+                    return (
+                      <option key={code} value={code}>
+                        {name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+
+            <div className="w-full mb-1">
+              <button
+                id="settingForm7text3"
+                className={`w-full ${
+                  color_scheme == "Red"
+                    ? "bg-[#DC4C64]"
+                    : color_scheme == "Green"
+                    ? "bg-[#14A44D]"
+                    : "bg-[#7A7A7A]"
+                }  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md ${
+                  isLoading ? "opacity-50" : null
+                }`}
+                onClick={handleSubmit}
+              >
+                {isLoading ? "saving..." : "Set as default Language for me"}
+              </button>
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 };
