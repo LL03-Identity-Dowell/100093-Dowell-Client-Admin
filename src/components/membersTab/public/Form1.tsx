152eacdef7debebb283ee3a79adacc3fd8ff1001
Nouman <nomi676767@gmail.com>
2024-01-11 13:37:28 +0500

    FIX RELOADING ISSUE IN PROFOLIO , ADDED QR CODE FORM IN PUBLIC TAB

--- src/components/membersTab/public/Form1.tsx
+++ src/components/membersTab/public/Form1.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/Store";
 import { toast } from "react-toastify";
-
+import images from "../../images";
 const Form1 = () => {
   const [formInputs, setFormInputs] = useState({
     public_count: 0,
@@ -87,72 +87,94 @@ const Form1 = () => {
     (state: RootState) => state.setting?.data?.color_scheme
   );
   return (
-    <>
-      <div className="lg:w-1/3 border border-[#54595F] card-shadow">
-        <i
-          id="publictext4"
-          className="text-[#FF0000] text-lg font-roboto font-semibold p-[30px] m-5 flex flex-col items-center"
-        >
-          Invite PUBLIC to my organisation
-        </i>
-        <form className="px-[30px] mb-8" onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              id="publictext5"
-              className="text-[#7A7A7A] text-lg font-roboto font-bold "
-            >
-              Number of Public Links needed now
-            </label>
-            <input
-              type="number"
-              placeholder="Number"
-              id="public_count"
-              min={1}
-              value={value}
-              onChange={handleOnChange}
-              className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              id="publictext6"
-              className="text-[#7A7A7A] text-lg font-roboto font-bold "
-            >
-              Create automatically if less than
-            </label>
-            <select
-              id="publicselect1"
-              className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
-            >
-              <option>5 </option>
-              <option> 10 </option>
-              <option>25 </option>
-              <option>50 </option>
-              <option>100 </option>
-              <option>500 </option>
-              <option>1000 </option>
-            </select>
-          </div>
-          <button
-            id="publictext7"
-            disabled={isLoading || publicAccess === "View"}
-            className={`w-full h-12  ${
-              isLoading == true
-                ? "bg-[#b8b8b8]"
-                : color_scheme == "Red"
-                ? "bg-[#DC4C64]"
-                : color_scheme == "Green"
-                ? "bg-[#14A44D]"
-                : "bg-[#7A7A7A]"
-            } mb-8 hover:bg-[#61CE70] rounded-[4px] text-white font-roboto`}
-          >
-            {" "}
-            {isLoading ? "loading..." : "Create Public Invitation Link"}
-          </button>
-        </form>
-      </div>
-    </>
-  );
+		<>
+			<div className="lg:w-1/3 border border-[#54595F] card-shadow">
+				<i
+					id="publictext4"
+					className="text-[#FF0000] text-lg font-roboto font-semibold p-[30px] m-5 flex flex-col items-center"
+				>
+					Invite PUBLIC to my organisation
+				</i>
+				<form className="px-[30px] mb-8" onSubmit={handleSubmit}>
+					<div className="mb-4">
+						<label
+							id="publictext5"
+							className="text-[#7A7A7A] text-lg font-roboto font-bold "
+						>
+							Number of Public Links needed now
+						</label>
+						<input
+							type="number"
+							placeholder="Number"
+							id="public_count"
+							min={1}
+							value={value}
+							onChange={handleOnChange}
+							className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+						/>
+					</div>
+					<div className="mb-4">
+						<label
+							id="publictext6"
+							className="text-[#7A7A7A] text-lg font-roboto font-bold "
+						>
+							Create automatically if less than
+						</label>
+						<select
+							id="publicselect1"
+							className="outline-none w-full h-12 px-4 rounded-sm border border-[#7A7A7A] bg-[#f5f5f5] text-[#7a7a7a] font-roboto"
+						>
+							<option>5 </option>
+							<option> 10 </option>
+							<option>25 </option>
+							<option>50 </option>
+							<option>100 </option>
+							<option>500 </option>
+							<option>1000 </option>
+						</select>
+					</div>
+					<button
+						id="publictext7"
+						disabled={isLoading || publicAccess === "View"}
+						className={`w-full h-12  ${
+							isLoading == true
+								? "bg-[#b8b8b8]"
+								: color_scheme == "Red"
+								? "bg-[#DC4C64]"
+								: color_scheme == "Green"
+								? "bg-[#14A44D]"
+								: "bg-[#7A7A7A]"
+						} mb-8 hover:bg-[#61CE70] rounded-[4px] text-white font-roboto`}
+					>
+						{" "}
+						{isLoading ? "loading..." : "Create Public Invitation Link"}
+					</button>
+				</form>
+
+				<form className=" px-4 flex flex-col items-center justify-center bg-[#f1f3f5] pb-4">
+					<div className="mb-4">
+						<img src={images.placeholder} alt="" />
+						<p id="teamtext61" className="text-center">
+							QR code for link
+						</p>
+					</div>
+					<button
+						id="publictext14"
+						// disabled={teamMemberAccess === "View"}
+						className={`w-full ${
+							color_scheme == "Red"
+								? "bg-[#DC4C64]"
+								: color_scheme == "Green"
+								? "bg-[#14A44D]"
+								: "bg-[#7A7A7A]"
+						}  hover:bg-[#61CE70] text-white  py-2 px-4 rounded-md`}
+					>
+						Download common invitation link QR code
+					</button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default Form1;
