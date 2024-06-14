93299f44348fa647ccd8fdd32149e4bf5140ea0c
unknown <hira43368@gmail.com>
2024-06-14 10:35:43 +0500

    school management page added with portfolio

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import {  Axios93Base } from "../api/axios";
 import Loader from "./whiteloader";
+import { ToastContainer, toast } from "react-toastify";
 
 interface userDetailProps {
-  Email:string; 
+  Email:string;
   Firstname:string; 
   Lastname: string;
   Phone:number;
@@ -17,23 +18,55 @@ interface userDetailProps {
   _id:string;
 }
 
+interface schoolClass {
+  class_name : string;
+  portfolio : string;
+  workspace_id : string;
+  _id : string
+}
+
 const UserDetails = () => {
 
   const [loading, setLoading] = useState(true);
   const [userDetail, setUserDetails] = useState<userDetailProps>();
-  const urlParams = new URLSearchParams(window.location.search);
-  const qrCodeId = urlParams.get("qrid");
+  const [portfolio, setportfolio] = useState(false)
+  const [portfolio_test, set_PortFolio_Test] = useState<schoolClass>()
+  const [bus_portfolio, set_bus_portfolio] = useState();
+  const [no_portfolio, set_no_portfolio] = useState(false)
 console.log(loading)
   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+  const qrCodeId = urlParams.get("qrid");
+  // get workspace id and portfolio from search param 
+  const urlParamsworkspaceid = urlParams.get("workspace_id");
+  const urlParamsportfolio = urlParams.get("portfolio");
+  if(urlParamsportfolio){
+    setportfolio(true)
+  }
+
     const fetchData = async () => {
       try {
         setLoading(true);
         const response = await Axios93Base.post("/getqrdetails", {
           qrid: qrCodeId, 
         });
-        console.log("type:", response.data);
         setUserDetails(response.data)
-        // Handle the response data as needed
+
+        const response_portfolio_test = await Axios93Base.post("/portfoliotest", {
+          portfolio: urlParamsportfolio,
+          owner_id: urlParamsworkspaceid
+      });
+      if (response_portfolio_test.data.hasOwnProperty('class')) {
+        set_PortFolio_Test(response_portfolio_test.data.class)
+        
+      } else if(response_portfolio_test.data.hasOwnProperty('bus')) {
+        set_bus_portfolio(response_portfolio_test.data.bus)
+      }
+      else {
+        set_no_portfolio(true)
+        toast.error("No Portfolio found in database")
+
+      }
       } catch (error) {
         // console.error("Error generating link:", error);
         // Handle the error
@@ -47,14 +80,110 @@ console.log(loading)
 
   return (
     <div>
-      {userDetail?   <div className="py-[5rem] lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
+      <ToastContainer position="top-right" />
+
+      {portfolio ? 
+      <div> {bus_portfolio && userDetail ?
+        <div>
+        {/* class form  */}
+        <div className="flex items-center justify-center h-screen">  
+            <div className="relative flex flex-col text-gray-700 p-5 bg-white shadow-lg bg-clip-border rounded-xl lg:w-[30rem] md:w-[30rem] w-full mx-auto">
+              <h1 className="text-center mb-3 font-dark">Bus Form</h1>
+              <p className="block mb-5 font-sans text-sm antialiased mx-auto font-normal leading-normal text-gray-900 opacity-75">
+              Do you want to mark attendance for {userDetail.Username}
+              </p>
+              <div className="p-6">
+                <div className="flex items-center justify-between mb-2">
+                  <button
+                    className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-75 bg-gray-900 text-gray-100 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    type="button"
+                    >
+                    Yes
+                  </button>
+                  <button
+                    className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-75 bg-gray-900 text-gray-100 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    type="button">
+                    No
+                  </button>
+
+                </div>
+               
+                 
+              </div>
+            
+                
+            </div>
+          </div>
+          
+        {/* class form  */}
+      </div>:portfolio_test && userDetail ? 
+      // bus form 
+      <div className="flex items-center justify-center h-screen">  
+            <div className="relative flex flex-col text-gray-700 p-5 bg-white shadow-lg bg-clip-border rounded-xl lg:w-[30rem] md:w-[30rem] w-full mx-auto">
+              <h1 className="text-center mb-3 font-dark">Class Form</h1>
+              <p className="block mb-5 font-sans text-sm antialiased mx-auto font-normal leading-normal text-gray-900 opacity-75">
+              Do you want to mark attendance for {userDetail.Username}
+              </p>
+              <div className="p-6">
+                <div className="flex items-center justify-between mb-2">
+                  <button
+                    className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-75 bg-gray-900 text-gray-100 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    type="button"
+                    >
+                    Yes
+                  </button>
+                  <button
+                    className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-75 bg-gray-900 text-gray-100 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    type="button">
+                    No
+                  </button>
+
+                </div>
+               
+                 
+              </div>
+            
+                
+            </div>
+          </div>
+      // bus form 
+      :no_portfolio ? 
+      <div className="flex items-center justify-center h-screen">  
+            <div className="relative flex flex-col text-gray-700 p-5 bg-white shadow-lg bg-clip-border rounded-xl lg:w-[30rem] md:w-[30rem] w-full mx-auto">
+              <h1 className="text-center mb-3 font-dark">No Portfolio</h1>
+              <p className="block mb-5 font-sans text-sm antialiased mx-auto font-normal leading-normal text-gray-900 opacity-75">
+              No Portfolio Found in database
+              </p>
+              <div className="p-6 text-center mx-auto">
+                
+                  <button
+                    className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-75 bg-gray-900 text-gray-100 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                    type="button">
+                    Cancel
+                  </button>
+
+               
+                 
+              </div>
+            
+                
+            </div>
+          </div>
+          
+      
+      :<Loader/>}</div>: <div>
+      {userDetail?<div className="py-[5rem] lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
 <div className="flex justify-center items-center">
   <div className="w-full sm:w-6/12 md:w-1/3 lg:w-2/4 rounded-lg shadow-md bg-white overflow-hidden">
-    <img className="w-full h-48 object-contain" src={userDetail.Profile_Image} alt="User Image"/>
+    <img className="w-full h-48 rounded-full object-contain" src={userDetail.Profile_Image} alt="User Image"/>
     <div className="p-4">
       <h5 className="text-xl font-bold tracking-tight text-green-400 text-center">Personal Details & Information</h5>
     </div>
     <dl className="divide-y divide-gray-100">
+    <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
+        <dt className="text-sm font-medium leading-6 text-gray-900">Username</dt>
+        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Username}</dd>
+      </div>
       <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
         <dt className="text-sm font-medium leading-6 text-gray-900">First name</dt>
         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Firstname}</dd>
@@ -63,52 +192,11 @@ console.log(loading)
         <dt className="text-sm font-medium leading-6 text-gray-900">Last name</dt>
         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Lastname}</dd>
       </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
+      <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
         <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Lastname}</dd>
+        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Email}</dd>
       </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Phone</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Phone}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Role</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Role}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Team code</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Team_Code}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Username</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.Username}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Company Id</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.company_id}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Document Id</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.document_id}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Phone Code</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail.phonecode}</dd>
-      </div>
-            <div className="py-6 sm:grid sm:grid-cols-3 sm:gap-4 lg:px-[5rem] md:px-[4rem] sm:px[2rem] xs:px=[3rem]">
-
-        <dt className="text-sm font-medium leading-6 text-gray-900">Id</dt>
-        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{userDetail._id}</dd>
-      </div>
-  
+     
     </dl>
   
   </div>
@@ -116,9 +204,10 @@ console.log(loading)
 
 
 
-</div>
-     :<Loader/>}
+</div>:<Loader/>}
+    </div>}
     </div>
+   
   );
 };
 
